4ebb75d04b5ca72ab096d14f8ae7e05d780ef42d
ym174 <ym1743kp@gmail.com>
2025-06-07 00:31:08 +0900

    2506070030

---
+++ src/features/firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA_m0qL_HWvhEjLUsKdzv760PhifOcczsU",
+    authDomain: "sophiaeats-1bac3.firebaseapp.com",
+    projectId: "sophiaeats-1bac3",
+    storageBucket: "sophiaeats-1bac3.firebasestorage.app",
+    messagingSenderId: "1082443186890",
+    appId: "1:1082443186890:web:3ee35161b1d47831a02a33",
+    measurementId: "G-2LPTTDTK3Z"
+};
