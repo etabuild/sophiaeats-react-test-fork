ea154bf100bd9b655fde1efc4c851f9e4dae76ae
ym174 <ym1743kp@gmail.com>
2025-06-09 11:20:22 +0900

    2506091120

--- src/route/Cart/Cart.tsx
+++ src/route/Cart/Cart.tsx
@@ -28,19 +28,37 @@ export const Cart = ({navigate, list}: CartProps) => {
                             <p className={"menuTitle"}>{item.name_ja}</p>
 
                             <p className={"menuPrice"}>¥{item.price}</p>
-                            <select name={`x${item.amount}`}>
+{/*                            <select name={`x${item.amount}`} onSelect={(select) => console.log(select)}>
                                 {[1, 2, 3, 4, 5].map((num) => {
                                     return(
                                     item.amount === num ? <option selected> x{num}</option>:<option>x{num}</option>
                                     )
                                 })}
-                            </select>
+                            </select>*/}
+                            <p className={"dropdown"}>x{item.amount}</p>
                             <button className={"deleteButton"}>
                                 <i className={"material-symbols-rounded"}>delete</i>
                             </button>
                         </div>
 
                     ))}
+{/*                    <div className={"inCartItem"} >
+                        <div style={{background: `url()`, backgroundSize: 'cover'}} className={"menuImg"}></div>
+                        <p className={"menuTitle"}>ロングオフピークカツカレー</p>
+
+                        <p className={"menuPrice"}>¥1000</p>
+                                                    <select name={`x${item.amount}`} onSelect={(select) => console.log(select)}>
+                                {[1, 2, 3, 4, 5].map((num) => {
+                                    return(
+                                    item.amount === num ? <option selected> x{num}</option>:<option>x{num}</option>
+                                    )
+                                })}
+                            </select>
+                        <p className={"dropdown"}>x9</p>
+                        <button className={"deleteButton"}>
+                            <i className={"material-symbols-rounded"}>delete</i>
+                        </button>
+                    </div>*/}
                 </div>
                 <div className={"totalPriceSection"}>
                     <p className={"totalPriceLabel"}>合計</p>
